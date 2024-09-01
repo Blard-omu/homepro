8cff63d4bebf8aedd00047636c5b082b178a17c2
BLARD <peteromu76@email.com>
2024-09-01 16:56:08 +0100

    SEO done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Listings from './pages/Listings';
-import Menu from './components/navs/Menu';
-import Blog from './pages/Blog';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Listings from "./pages/Listings";
+import Menu from "./components/navs/Menu";
+import Blog from "./pages/Blog";
+import HelmetWrapper from "./components/resusables/HelmetAsync";
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Menu/>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/about' element={<About/>} />
-      <Route path='/contact' element={<Contact/>} />
-      <Route path='/listings' element={<Listings/>} />
-      <Route path='/blog' element={<Blog/>} />
-    </Routes>
-    </BrowserRouter>
-  )
-}
+    <HelmetWrapper>
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/listings" element={<Listings />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetWrapper>
+  );
+};
 
-export default App
+export default App;

5e4e6f8451e0557288477c6650307c564fdcb173
BLARD <peteromu76@email.com>
2024-10-14 00:06:47 +0100

    card correction

--- src/components/cards/PropertyCard.jsx
+++ src/components/cards/PropertyCard.jsx
@@ -1,3 +1,78 @@
+// import React from "react";
+// import ngnIcon from "../../assets/icons/CurrencyNgn.png";
+// import heart from "../../assets/icons/Heart.png";
+// import bedIcon from "../../assets/icons/Bed.png";
+// import bathIcon from "../../assets/icons/Bathtub.png";
+// import noImage from "../../assets//images/broken-image.webp";
+// import tdesign from "../../assets/icons/tdesign_measurement-2.png";
+// import { Link } from "react-router-dom";
+// const PropertyCard = ({
+//   _id,
+//   title,
+//   image = `${noImage}`,
+//   price,
+//   bed,
+//   bath,
+//   size,
+// }) => {
+//   return (
+//     <>
+//       <Link to={`/detail/${_id}`}>
+//         <div
+//           className="rounded-2xl border border-slate-300 max-w-[400px] text-dark hover:shadow-lg shadow-primary-foreground hover:border-primary"
+//           key={_id}
+//         >
+//           <div className="min-h-[396px] p-2 2xl:p-6 rounded-2xl min-w-[355px]">
+//            <div className="">
+//            <img src={image} alt="" className="w-full h-[245px] rounded-xl" />
+//            </div>
+//             <div className="flex text-[#1F1F1F] justify-between pt-2 pr-2">
+//               <div className="flex">
+//                 <img src={ngnIcon} alt="ngn" className="" />
+//                 <span className="font-semibold">
+//                   {price.toLocaleString("en-us")}
+//                 </span>
+//               </div>
+//               <img src={heart} alt="vector" className="w-[20px] h-[20px]" />
+//             </div>
+//             <div className="text-[#1F1F1F] weight-[700] text-xl font-semibold pt-2">
+//               <h3 className="font-bold">
+//                 {title && title.length > 28
+//                   ? `${title.slice(0, 25)}...`
+//                   : title}
+//               </h3>
+//             </div>
+//             <div className="w-full justify-between flex text-[#1F1F1F]">
+//               <div className="w-[25%] text-[#1F1F1F] flex items-center">
+//                 <div className="w-[50%]">
+//                   <img src={bedIcon} alt="bed" className="" />
+//                 </div>
+//                 <span className="w-[50%] text-sm">{bed} bed</span>
+//               </div>
+
+//               <div className="w-[25%] text-[#1F1F1F] flex items-center">
+//                 <div className="w-[50%]">
+//                   <img src={bathIcon} alt="bed" className="" />
+//                 </div>
+//                 <span className="w-[50%] text-sm">{bath} bed</span>
+//               </div>
+
+//               <div className="w-[35%] text-[#1F1F1F] flex  items-center">
+//                 <div className="w-[30%]">
+//                   <img src={tdesign} alt="bed" className="" />
+//                 </div>
+//                 <span className="w-[70%] text-sm">{size} sq ft</span>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </Link>
+//     </>
+//   );
+// };
+
+// export default PropertyCard;
+
 import React from "react";
 import ngnIcon from "../../assets/icons/CurrencyNgn.png";
 import heart from "../../assets/icons/Heart.png";
@@ -6,6 +81,7 @@ import bathIcon from "../../assets/icons/Bathtub.png";
 import noImage from "../../assets//images/broken-image.webp";
 import tdesign from "../../assets/icons/tdesign_measurement-2.png";
 import { Link } from "react-router-dom";
+
 const PropertyCard = ({
   _id,
   title,
@@ -19,12 +95,14 @@ const PropertyCard = ({
     <>
       <Link to={`/detail/${_id}`}>
         <div
-          className="rounded-2xl border border-slate-300 max-w-[400px] text-dark hover:shadow-lg shadow-primary-foreground hover:border-primary"
+          className="rounded-2xl border border-slate-300 w-[355px] 2xl:w-[400px] text-dark hover:shadow-lg shadow-primary-foreground hover:border-primary"
           key={_id}
         >
-          <div className="min-h-[396px] p-2 2xl:p-6 rounded-2xl min-w-[375px]">
-            <img src={image} alt="" className="w-full h-[245px] rounded-xl" />
-            <div className="flex text-[#1F1F1F] justify-between pt-2">
+          <div className="min-h-[396px] p-2 2xl:p-6 rounded-2xl min-w-[355px]">
+            <div className="flex justify-center items-center h-[245px]">
+              <img src={image} alt="" className="w-full h-full rounded-xl" />
+            </div>
+            <div className="flex text-[#1F1F1F] justify-between pt-2 pr-2">
               <div className="flex">
                 <img src={ngnIcon} alt="ngn" className="" />
                 <span className="font-semibold">
@@ -50,14 +128,14 @@ const PropertyCard = ({
 
               <div className="w-[25%] text-[#1F1F1F] flex items-center">
                 <div className="w-[50%]">
-                  <img src={bathIcon} alt="bed" className="" />
+                  <img src={bathIcon} alt="bath" className="" />
                 </div>
                 <span className="w-[50%] text-sm">{bath} bed</span>
               </div>
 
-              <div className="w-[35%] text-[#1F1F1F] flex  items-center">
+              <div className="w-[35%] text-[#1F1F1F] flex items-center">
                 <div className="w-[30%]">
-                  <img src={tdesign} alt="bed" className="" />
+                  <img src={tdesign} alt="size" className="" />
                 </div>
                 <span className="w-[70%] text-sm">{size} sq ft</span>
               </div>
--- src/components/home/Featured.jsx
+++ src/components/home/Featured.jsx
@@ -7,7 +7,7 @@ const FeaturedProperty = () => {
   return (
     <ComponentWrapper className="my-64">
       <h1 className="text-3xl text-center lg:text-start md:text-4xl font-bold py-8">Featured Properties</h1>
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 2xl-gap-8 justify-between">
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 2xl-gap-8 justify-between place-items-center">
         {featureDb &&
           featureDb.length > 0 &&
           featureDb.map((property) => {
